7680c189bd5b87c0df512802d9f6fceb283fc50e
dosisido <dosisido46@gmail.com>
2024-12-03 10:45:17 +0100

    2024/03 added test

--- 2024/03/src/main.rs
+++ 2024/03/src/main.rs
@@ -7,10 +7,8 @@ const REGEX_STR_2: &str= r#"mul\((?<op1>\d{1,4}),(?<op2>\d{1,4})\)|do\(\)|don't\
 const REGEX_STR_2_1: &str = r#"(?<mul>mul)\((?<op1>\d{1,4}),(?<op2>\d{1,4})\)|(?<do>do\(\))|(?<dont>don't\(\))"#;
 
 
-fn es1(file_data: &str) -> (){
-    let re = Regex::new(REGEX_STR_1).unwrap();
-
-    let res = re
+fn es1(file_data: &str) -> i32{
+    Regex::new(REGEX_STR_1).unwrap()
         .captures_iter(&file_data)
         .map(|m| 
             (
@@ -19,12 +17,10 @@ fn es1(file_data: &str) -> (){
             )
         )
         .map(|(op1, op2)| op1*op2)
-        .sum::<i32>();
-
-    println!("point1 {}", res);
+        .sum::<i32>()
 }
 
-fn es2(file_data: &str) -> (){
+fn es2(file_data: &str) -> i32{
     let re= Regex::new(REGEX_STR_2).unwrap();
     let data: Vec<&str> = re
         .find_iter(&file_data)
@@ -60,37 +56,35 @@ fn es2(file_data: &str) -> (){
         }
         i+=1;
     }
-    println!("point2 {}", result);
+    result
 }
 
-fn es2_1(file_data: &str) -> (){
-    let result = 
-        Regex::new(REGEX_STR_2_1).unwrap()
-        .captures_iter(&file_data)
-        .scan(1, |to_do, el: Captures<'_>| {
-            match el.name("mul") {
-                Some(_) => {
-                    if *to_do == 0{
-                        return Some(0);
-                    }
-                    Some(
-                        el.name("op1").unwrap().as_str().parse::<i32>().unwrap() *
-                        el.name("op2").unwrap().as_str().parse::<i32>().unwrap()
-                    )
-                },
-                None => {
-                    if let Some(_) = el.name("do") {
-                        *to_do = 1;
-                    }
-                    if let Some(_) = el.name("dont") {
-                        *to_do = 0;
-                    }
-                    Some(0)
+fn es2_1(file_data: &str) -> i32{
+    Regex::new(REGEX_STR_2_1).unwrap()
+    .captures_iter(&file_data)
+    .scan(1, |to_do, el: Captures<'_>| {
+        match el.name("mul") {
+            Some(_) => {
+                if *to_do == 0{
+                    return Some(0);
                 }
+                Some(
+                    el.name("op1").unwrap().as_str().parse::<i32>().unwrap() *
+                    el.name("op2").unwrap().as_str().parse::<i32>().unwrap()
+                )
+            },
+            None => {
+                if let Some(_) = el.name("do") {
+                    *to_do = 1;
+                }
+                if let Some(_) = el.name("dont") {
+                    *to_do = 0;
+                }
+                Some(0)
             }
-        })
-        .sum::<i32>();
-    println!("point2_1 {}", result);
+        }
+    })
+    .sum::<i32>()
 }
 
 fn main() {
@@ -98,9 +92,28 @@ fn main() {
     let file_data: String = fs::read_to_string(INPUT)
         .expect("Error opening the file");
 
-    es1(&file_data);
-    es2(&file_data);
-    es2_1(&file_data);
+    println!("point1 {}", es1(&file_data));
+    println!("point2 {}", es2(&file_data));
+    println!("point2_1 {}", es2_1(&file_data));
 
+}
+
+
+
+#[cfg(test)]
+mod test{
+    use super::*;
+    const TEST_DATA: &str = include_str!(r#"..\test.txt"#);
+
+    #[test]
+    fn es1_test(){
+        assert_eq!(es1(&TEST_DATA), 161);
+    }
+
+    #[test]
+    fn es2_test(){
+        assert_eq!(es2(&TEST_DATA), 48);
+        assert_eq!(es2_1(&TEST_DATA), 48);
+    }
 
 }
